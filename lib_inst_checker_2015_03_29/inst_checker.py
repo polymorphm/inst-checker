eb41344afce5969a5f90fd3cd50d74a1f9f91dda
Andrej Antonov <polymorphm@gmail.com>
2015-04-17 18:25:17 +0300

    find and fetch confirm_url

--- lib_inst_checker_2015_03_29/inst_checker.py
+++ lib_inst_checker_2015_03_29/inst_checker.py
@@ -7,6 +7,10 @@ from urllib import request as url_request
 from http import cookiejar
 import json
 import time
+import socket
+import imaplib
+from email import parser as email_parser
+import re
 
 INSTAGRAM_DOMAIN = 'instagram.com'
 INSTAGRAM_URL = 'https://{}/'.format(INSTAGRAM_DOMAIN)
@@ -19,6 +23,67 @@ class InstCheckerError(Exception):
 
 class InstCheckerCtx:
     pass
+
+class SafeIMAP4(imaplib.IMAP4):
+    def _create_socket(self):
+        sock = socket.create_connection(
+                (self.host, self.port),
+                timeout=15.0,
+                )
+        
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        
+        return sock
+
+def mail_fetch(email, imap_host, email_login, email_pass):
+    try:
+        imap = SafeIMAP4(host=imap_host)
+        imap.starttls()
+        imap.login(email_login, email_pass)
+        try:
+            imap.select()
+            typ, search_data = imap.search(None, 'UNSEEN')
+            
+            for num in reversed(search_data[0].split()):
+                typ, fetch_data = imap.fetch(num, '(RFC822)')
+                
+                msg_parser = email_parser.BytesFeedParser()
+                msg_parser.feed(fetch_data[0][1])
+                msg = msg_parser.close()
+                
+                msg_from = msg.get_all('from')
+                msg_to = msg.get_all('to')
+                msg_subject = msg.get_all('subject')
+                
+                print('######', 'msg_from: ', msg_from)
+                print('######', 'msg_to: ', msg_to)
+                print('######', 'msg_subject: ', msg_subject)
+                
+                if not msg_from or tuple(msg_from) != ('"Instagram" <no-reply@mail.instagram.com>',) or \
+                        not msg_to or tuple(msg_to) != (email,) or \
+                        not msg_subject or tuple(msg_subject) != ('Confirm your email address for Instagram',):
+                    continue
+                
+                print('######', 'YESSS!!')
+                
+                for msg_part in msg.walk():
+                    if msg_part.get_content_type() == 'text/plain':
+                        payload = msg_part.get_payload(decode=True)
+                        
+                        assert isinstance(payload, bytes)
+                        
+                        msg_text = payload.decode(errors='replace')
+                        
+                        return msg_text
+        finally:
+            imap.close()
+            imap.logout()
+    except imaplib.IMAP4.error as imap_error:
+        error_str = 'email is {!r}, error is {!r}'.format(
+                email,
+                imap_error,
+                )
+        raise imaplib.IMAP4.error(error_str)
 
 def unsafe_check(inst_checker_ctx):
     ua_name = inst_checker_ctx.ua_name
@@ -160,10 +225,71 @@ def unsafe_edit(inst_checker_ctx):
     inst_checker_ctx.new_email = new_email
     inst_checker_ctx.is_edit_begin = True
     
+    mail_ru_imap_host = 'imap.mail.ru'
+    yandex_ru_imap_host = 'imap.yandex.ru'
     
+    if new_email.endswith('@mail.ru') or \
+            new_email.endswith('@inbox.ru') or \
+            new_email.endswith('@list.ru') or \
+            new_email.endswith('@bk.ru'):
+        email_login = new_email
+        imap_host = mail_ru_imap_host
+    elif new_email.endswith('@yandex.ru'):
+        email_login = new_email
+        imap_host = yandex_ru_imap_host
+    else:
+        raise InstCheckerError('unknown email service')
     
+    for att_i in range(10):
+        time.sleep(10)
+        
+        try:
+            mail_text = mail_fetch(new_email, imap_host, email_login, new_email_password)
+        except socket.timeout:
+            continue
+        
+        if mail_text is None:
+            continue
+        
+        assert isinstance(mail_text, str)
+        
+        confirm_url_prefix = 'https://instagram.com/accounts/confirm_email/'
+        confirm_url_match = re.search(
+                r'\[(?P<confirm_url>' + re.escape(confirm_url_prefix) + r'\S+)\]',
+                mail_text,
+                flags=re.S,
+                )
+        
+        if confirm_url_match is None:
+            continue
+        
+        confirm_url = confirm_url_match.group('confirm_url')
+        
+        break
+    else:
+        raise InstCheckerError(
+                'confirm_url not received',
+                )
     
-    #inst_checker_ctx.is_edit = True
+    print('#####', 'confirm_url: ', confirm_url)
+    
+    resp = open_func(
+        opener,
+        url_request.Request(
+            confirm_url,
+            headers={
+                'User-Agent': ua_name,
+            },
+        ),
+        timeout=REQUEST_TIMEOUT,
+    )
+    
+    if resp.getcode() != 200 or resp.geturl() != confirm_url:
+        raise InstCheckerError('fetch confirm_url error')
+    
+    print('***', 'fetch confirm_url OK', '***')
+    
+    inst_checker_ctx.is_edit = True
 
 def init_inst_checker_ctx(inst_checker_ctx, ua_name, username, password):
     cookies = cookiejar.CookieJar()
